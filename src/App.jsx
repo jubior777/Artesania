05b49cbc6db3bdc8f2579c6ecc0eb43007e91565
jorge <jubior777@hotmail.com>
2025-04-24 08:16:45 -0500

    primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <p>Este es el sitio de desarrollo</p>
+      </div>
+    </>
+  )
+}
+
+export default App
