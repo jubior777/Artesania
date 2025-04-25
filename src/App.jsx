3dc6268048eb0f86a350da9d4eeeab1dade802ae
jorge <jubior777@hotmail.com>
2025-04-25 18:24:58 -0500

    nuevas funciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import{API_URL} from "../constants/env"
+import{API_URL} from "../env"
 
 import './App.css'
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './Router/router.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+
+  
+
 )
