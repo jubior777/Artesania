9ca7cb315076fc5b442e6f5037f018425574aa61
jorge <jubior777@hotmail.com>
2025-04-25 15:53:17 -0500

    actualizar archivo 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import { React } from 'react'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello World Paz  </h1>
+        <h1>Hello World vamos bien</h1>
         <p>https://api-ecommerce.ed.team/api/v1</p>
       </div>
     </>
