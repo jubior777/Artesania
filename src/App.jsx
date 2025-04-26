839b71ae7cbd00f5bf73490a72cd8d0fa6609dfa
jorge <jubior777@hotmail.com>
2025-04-26 17:09:49 -0500

    actual

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { API_URL } from "./components/pages/MainHeader"; // Ajusta la ruta de MainHeader.jsx
+import {API_URL} from "../../constants/env"; // Ajusta la ruta de env.js
 import MainHeader from "./components/pages/MainHeader"; // Ajusta la ruta de MainHeader.jsx
 
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import {API_URL} from "./pages/MainHeader"; // Asegúrate de que la ruta sea correcta
-import MainHeader from "./pages/MainHeader"; // Asegúrate de que la ruta sea correcta
 import Products from "./Products"; // Asegúrate de que la ruta sea correcta
 
 const HomePage = () => {
@@ -8,11 +6,7 @@ const HomePage = () => {
     <>
       <MainHeader /> 
       <div>
-        <ul>
-          <li>Inicio</li>
-          <li>Producto</li>
-          <li>Contactos</li>
-        </ul>
+        
         <Products /> {/* Aquí se utiliza el componente Products */}
       </div>
     </>
